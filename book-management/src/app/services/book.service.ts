bcda696822ccc9b23f581f62a5538c9eccf2ae6b
kalpa Perera <klperera660@gmail.com>
2025-05-23 08:32:10 +0530

    Book service for API calls

--- book-management/src/app/services/book.service.ts
+++ book-management/src/app/services/book.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/books';
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.baseUrl);
+  }
+
+  GetBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.baseUrl}/${id}`);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.baseUrl, book);
+  }
+
+  updateBook(id: number, book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.baseUrl}/${id}`, book);
+  }
+
+  deleteBook(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
